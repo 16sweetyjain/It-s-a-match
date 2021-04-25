523081750154e6b3ad8c1165cf8ac1baa21246e4
16sweetyjain <16jainsweety@gmail.com>
2021-04-25 16:39:15 +0530

    defined HomePage for client

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import {Component} from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
-function App() {
+import SignIn from './components/SignIn.js';
+import Signup from './components/Signup.js';
+
+export  class App extends Component{
+  render(){
   return (
     <div>
-      Welcome to Tinder for Dogs
+      <Route to='/signin' component={SignIn}/>
+      <Route to='/signup' component={Signup}/>
+      <div>
+      Welcome to It-s-a-match.We hope you find the perfect match for your pet.
+      Start exploring now.
+    </div>
+    <Link to='/signin'>login</Link>
+    <Link to='/signup'>register</Link>
     </div>
   );
+  }
+
 }
+
 
 export default App;
---
+++ client/frontend/src/components/SignIn.js
@@ -0,0 +1,18 @@
+
+import { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import {TextInput} from 'react-materialize';
+
+export default class SignIn extends Component {
+    render(){
+  return (
+    <div>
+        <div>Login</div>
+      
+    </div>
+
+  );
+    }
+}
+
+
